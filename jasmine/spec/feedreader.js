303b869f4fb9ef8d381e24a333867b5b6af5ac08
Mustafa Khamisi <mustafangure95@gmail.com>
2019-04-11 22:16:12 +0300

    fix: Correct words used in the comments

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -4,20 +4,20 @@
 /* feedreader.js
  *
  * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
+ * all of the tests that will be run against the application.
  */
 
-/* We're placing all of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
+/* I am placing all of my tests within the $() function,
+ * since some of these tests may require DOM elements. I want
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
-    /* This is our first test suite - a test suite just contains
+    /* This is my first test suite - a test suite just contains
     * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
+    * feeds definitions, the allFeeds variable in my application.
     */
     describe('RSS Feeds', () => {
-        /* This is our first test - it tests to make sure that the
+        /* This is my first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
          * empty. Experiment with this before you get started on
          * the rest of this project. What happens when you change
@@ -28,7 +28,6 @@ $(function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
-
 
         /* Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
@@ -41,8 +40,6 @@ $(function() {
             }
         });
 
-
-
         /* Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
@@ -54,7 +51,6 @@ $(function() {
             }
         });
     });
-
 
     /* Write a new test suite named "The menu" */
     describe('The menu', () => {
